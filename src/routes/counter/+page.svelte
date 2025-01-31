3f02a393c875482118a3160f9723065bd2f3dbf3
Josh Stauffer <joshstauffer@gmail.com>
2025-01-30 20:32:59 -0600

    first commit

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  let count = $state(0);
+</script>
+
+<button onclick={count++}>The count is {count}</button>
+
+<h3>What the code looks like:</h3>
+
+<pre><code>
+  &lt;script&gt;
+    let count = $state(0);
+  &lt;/script&gt;
+  
+  &lt;button onclick={"{"}count++{"}"}&gt;Count = {"{"}count{"}"}&lt;/button&gt;
+
+  &lt;style&gt;
+    button {"{"}
+      font-size: 4rem;
+    {"}"}
+  &lt;/style&gt;
+</code></pre>
+
+<style>
+  button {
+    font-size: 4rem;
+  }
+</style>
